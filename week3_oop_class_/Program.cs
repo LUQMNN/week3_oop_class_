01e852a90cd3a861f6df726f2b5697fab4e6ad36
luqma <luqma@LQMNN_>
2024-09-18 08:55:55 +0800

    Add project files.

---
+++ week3_oop_class_/Program.cs
@@ -0,0 +1,20 @@
+﻿using week3_oop_class_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // creating an object (variable of a certain class type) 
+        transaction transaction = new transaction();
+
+        transaction.month = 1;
+        transaction.val = 10.5f;
+
+        Console.WriteLine("Transaction value is " + transaction.val);
+
+        float val = 10.5f;
+        Console.WriteLine("Transaction value is "+ val);
+
+
+    }
+}
